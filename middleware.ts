9fbe589d6366fec5262ae322ee62027aa78358c1
José Daza Díaz <josedaza9@gmail.com>
2023-04-24 14:33:30 -0500

    :fire: Remove import * as process from 'process'; from middleware

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { getToken } from 'next-auth/jwt';
-import * as process from 'process';
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(req: NextRequest, res: NextResponse) {
