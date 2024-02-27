d920f38dde41355a0cd22468bc72ad2bb651fd6d
José Daza Díaz <josedaza9@gmail.com>
2024-02-27 08:28:39 -0500

    :bug: Fix client side module

--- components/plants/CreateEntry/CreateEntry.tsx
+++ components/plants/CreateEntry/CreateEntry.tsx
@@ -1,24 +1,33 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import {
    Box,
    Button,
+   Card,
+   CardActionArea,
+   CardMedia,
+   Chip,
    CircularProgress,
+   FormLabel,
    Grid,
+   IconButton,
    TextField,
    Typography,
 } from '@mui/material';
 import './CreateEntry.css';
 import { convertToRaw, EditorState } from 'draft-js';
-import dynamic from 'next/dynamic'; // Importa 'dynamic' de 'next/dynamic'
+import dynamic from 'next/dynamic';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import { useForm } from 'react-hook-form';
+import { UploadOutlined } from '@mui/icons-material';
+import { CloseIcon } from 'next/dist/client/components/react-dev-overlay/internal/icons/CloseIcon';
 
 type FormData = {
    plantName: string;
    description: string;
    category: string;
+   cover: File | undefined;
 };
 
 const Editor = dynamic(
@@ -27,19 +36,46 @@ const Editor = dynamic(
 );
 
 const CreateEntry = () => {
+   const fileInputRef = useRef<HTMLInputElement>(null);
+
    const [editorState, setEditorState] = useState(EditorState.createEmpty());
    const [loading, setLoading] = useState(false);
    const {
       register,
       handleSubmit,
       formState: { errors },
+      getValues,
+      setValue,
+      watch,
    } = useForm<FormData>();
    const onSubmit = (data: any) => {
       const rawContentState = convertToRaw(editorState.getCurrentContent());
-      const stringifiedContent = JSON.stringify(rawContentState);
-      console.log(stringifiedContent);
-      // Aquí tendrías el estado del editor como `data.description`.
-      // Puedes convertirlo al formato que necesites para tu backend.
+      const stringifyContent = JSON.stringify(rawContentState);
+      console.log({ data });
+   };
+
+   const onFileSelected = async ({
+      target,
+   }: React.ChangeEvent<HTMLInputElement>) => {
+      if (!target.files || target.files.length === 0) return;
+
+      try {
+         const files = Array.from(target.files);
+         const file = files[0];
+         const formData = new FormData();
+         formData.append('file', file);
+         setValue('cover', file, {
+            shouldValidate: true,
+         });
+      } catch (e) {
+         console.log({ e });
+      }
+   };
+
+   const handleRemoveCover = () => {
+      setValue('cover', undefined, {
+         shouldValidate: true,
+      });
    };
 
    return (
@@ -96,6 +132,67 @@ const CreateEntry = () => {
                               },
                            }}
                         />
+                     </Box>
+                  </Grid>
+                  <Grid item xs={12}>
+                     <Box display="flex" flexDirection="column">
+                        <FormLabel sx={{ mb: 1 }}>Portada</FormLabel>
+                        <Button
+                           color="secondary"
+                           fullWidth
+                           startIcon={<UploadOutlined />}
+                           sx={{ mb: 3 }}
+                           onClick={() => fileInputRef.current?.click()}
+                        >
+                           Cargar imagen
+                        </Button>
+                        <input
+                           ref={fileInputRef}
+                           type="file"
+                           multiple
+                           accept="image/png, image/jpeg, image/gif"
+                           style={{ display: 'none' }}
+                           onChange={onFileSelected}
+                        />
+
+                        <Chip
+                           label="Es necesario cargar la imagen"
+                           color="error"
+                           variant="outlined"
+                           className="fadeIn"
+                           sx={{
+                              display:
+                                 getValues('cover') == null ? 'flex' : 'none',
+                           }}
+                        />
+
+                        {getValues('cover') && (
+                           /*Centrar card con un width del 50%*/
+                           <Card
+                              sx={{
+                                 width: '90%',
+                                 margin: 'auto',
+                                 position: 'relative',
+                              }}
+                           >
+                              <CardMedia
+                                 component="img"
+                                 className="fadeIn"
+                                 image={URL.createObjectURL(getValues('cover') as File)}
+                                 alt="cover"
+                                 style={{
+                                    width: '100%',
+                                    height: 'auto',
+                                 }}
+                              />
+                              <IconButton
+                                 onClick={handleRemoveCover}
+                                 sx={{ position: 'absolute', top: 0, right: 0 }}
+                              >
+                                 <CloseIcon />
+                              </IconButton>
+                           </Card>
+                        )}
                      </Box>
                   </Grid>
                   <Grid item xs={12}>
